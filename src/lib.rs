2a5f77a16f1b1bbfc3a9db9b96db11ecfff22d64
Kyle <me@khanson.io>
2020-12-21 22:18:57 -0800

    Clean up

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,11 @@
-#[macro_use]
-use serde;
 use chrono;
+pub use chrono::{DateTime, FixedOffset};
+use serde;
 use serde::de::DeserializeOwned;
-#[macro_use]
 pub use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 pub use wapc_guest::prelude;
 pub use wapc_guest::prelude::{console_log, host_call};
-pub use chrono::{DateTime, FixedOffset};
-
 
 #[macro_export]
 macro_rules! register_view {
@@ -33,7 +30,8 @@ macro_rules! register_view {
         crate::prelude::register_function(
             &["view-msg-", stringify!($n)].join("")[..],
             |b: &[u8]| {
-                let (state, topic, name, payload): (&[u8], &str, &str, &[u8]) = crate::deserialize(b)?;
+                let (state, topic, name, payload): (&[u8], &str, &str, &[u8]) =
+                    crate::deserialize(b)?;
                 let mut s: $t = crate::deserialize(state)?;
                 s.message(topic, name, payload)?;
                 serialize(&s)
@@ -47,6 +45,33 @@ macro_rules! register_view {
                 Ok(html.into_bytes())
             },
         );
+    }};
+}
+#[macro_export]
+macro_rules! register_root_view {
+    ($t:ident) => {{
+        crate::prelude::register_function(&"view-start-", |b: &[u8]| {
+            let params = crate::deserialize(b)?;
+            let s = $t::start(params)?;
+            serialize(&s)
+        });
+        crate::prelude::register_function(&"view-event-", |b: &[u8]| {
+            let (state, name, payload): (&[u8], &str, &[u8]) = crate::deserialize(b)?;
+            let mut s: $t = crate::deserialize(state)?;
+            s.event(name, payload)?;
+            serialize(&s)
+        });
+        crate::prelude::register_function(&"view-msg-", |b: &[u8]| {
+            let (state, topic, name, payload): (&[u8], &str, &str, &[u8]) = crate::deserialize(b)?;
+            let mut s: $t = crate::deserialize(state)?;
+            s.message(topic, name, payload)?;
+            serialize(&s)
+        });
+        crate::prelude::register_function(&"view-render-", |b: &[u8]| {
+            let s: $t = crate::deserialize(b)?;
+            let html = s.render()?;
+            Ok(html.into_bytes())
+        });
     }};
 }
 #[macro_export]
@@ -67,7 +92,6 @@ macro_rules! assemble_init {
         }
     };
 }
-
 
 pub fn hook(info: &std::panic::PanicInfo) {
     let msg = info.to_string();
@@ -136,17 +160,26 @@ where
 }
 
 #[derive(Deserialize, Serialize, Clone)]
-pub struct ScanOpts {
-  limit: u32,
-  reverse: bool,
-  start_key: Option<String>
+pub struct User {
+    id: String,
 }
 
+#[derive(Deserialize, Serialize, Clone)]
+pub struct ScanOpts {
+    limit: u32,
+    reverse: bool,
+    start_key: Option<String>,
+}
 
 impl Default for ScanOpts {
-    fn default() -> ScanOpts { ScanOpts { limit: 100, reverse: false, start_key: None } }
+    fn default() -> ScanOpts {
+        ScanOpts {
+            limit: 100,
+            reverse: false,
+            start_key: None,
+        }
+    }
 }
-
 
 impl ScanOpts {
     pub fn limit(&self, limit: u32) -> ScanOpts {
@@ -166,14 +199,18 @@ impl ScanOpts {
     }
 }
 
-pub fn utc_now() -> Result<DateTime<FixedOffset>>
-{
+pub fn utc_now() -> Result<DateTime<FixedOffset>> {
     let res = host_call("v1", "time", "UTC_NOW", &serialize(&())?)?;
-    let st: &str = deserialize(&res[..])?;
+    let st: &str = deserialize(&res)?;
     match DateTime::parse_from_rfc3339(st) {
         Ok(v) => Ok(v),
-        Err(e) => Err(e.into())
+        Err(e) => Err(e.into()),
     }
+}
+
+pub fn current_user() -> Result<Option<User>> {
+    let res = host_call("v1", "user", "USER", &serialize(&())?)?;
+    deserialize(&res)
 }
 
 pub fn kv_get<T>(b: &str, k: &str) -> Result<Option<T>>
@@ -181,7 +218,7 @@ where
     T: DeserializeOwned,
 {
     let res = host_call("v1", "kv", "GET", &serialize(&(b, k))?)?;
-    deserialize(&res[..])
+    deserialize(&res)
 }
 
 pub fn kv_scan<T>(b: &str, opts: &ScanOpts) -> Result<Vec<(String, T)>>
@@ -189,7 +226,7 @@ where
     T: DeserializeOwned,
 {
     let res = host_call("v1", "kv", "SCAN", &serialize(&(b, opts))?)?;
-    deserialize(&res[..])
+    deserialize(&res)
 }
 
 pub fn kv_set<T>(b: &str, k: &str, obj: &T) -> Result<()>
@@ -213,21 +250,19 @@ pub fn kv_delete(b: &str, k: &str) -> Result<()> {
     Ok(())
 }
 
-
 pub fn kv_delete_keys(b: &str, k: &str, v: Vec<String>) -> Result<()> {
     host_call("v1", "kv", "DEL_KEYS", &serialize(&(b, k, v))?)?;
     Ok(())
 }
 
-
 pub fn pubsub_subscribe(k: &str) -> Result<()> {
     let res = host_call("v1", "pubsub", "SUB", &serialize(k)?)?;
-    deserialize(&res[..])
+    deserialize(&res)
 }
 
 pub fn pubsub_unsubscribe(k: &str) -> Result<()> {
     let res = host_call("v1", "pubsub", "UNSUB", &serialize(&(k,))?)?;
-    deserialize(&res[..])
+    deserialize(&res)
 }
 
 pub fn pubsub_publish<T>(k: &str, event: &str, v: &T) -> Result<()>
@@ -236,7 +271,7 @@ where
 {
     let s: serde_bytes::ByteBuf = serde_bytes::ByteBuf::from(serialize(v)?);
     let res = host_call("v1", "pubsub", "PUB", &serialize(&(k, event, s))?)?;
-    deserialize(&res[..])
+    deserialize(&res)
 }
 
 pub fn pubsub_publish_from<T>(k: &str, event: &str, v: &T) -> Result<()>
@@ -245,7 +280,7 @@ where
 {
     let s: serde_bytes::ByteBuf = serde_bytes::ByteBuf::from(serialize(v)?);
     let res = host_call("v1", "pubsub", "PUB_FROM", &serialize(&(k, event, s))?)?;
-    deserialize(&res[..])
+    deserialize(&res)
 }
 
 pub fn presence_track<T>(topic: &str, key: &str, v: &T) -> Result<()>
@@ -254,14 +289,16 @@ where
 {
     let s: serde_bytes::ByteBuf = serde_bytes::ByteBuf::from(serialize(v)?);
     let res = host_call("v1", "presence", "TRACK", &serialize(&(topic, key, s))?)?;
-    deserialize(&res[..])
+    deserialize(&res)
 }
 
 pub fn presence_list<T>(topic: &str) -> Result<HashMap<String, Vec<T>>>
 where
-    T: DeserializeOwned
+    T: DeserializeOwned,
 {
     let res = host_call("v1", "presence", "LIST", &serialize(&(topic,))?[..])?;
-    deserialize::<_, Vec<serde_bytes::ByteBuf>>(&res)?.into_iter().map(|x| { deserialize(&x) }).collect()
+    deserialize::<_, Vec<serde_bytes::ByteBuf>>(&res)?
+        .into_iter()
+        .map(|x| deserialize(&x))
+        .collect()
 }
-
